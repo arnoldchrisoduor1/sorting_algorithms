55ca797896d9128ce722a5eff9217358d36ed04e
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2023-01-24 01:29:58 -0800

    initial commit

---
+++ updates.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+
+echo "<>" > 100-shell_sort.c
+
+echo "<>" > 101-cocktail_sort_list.c
+
+echo "<>" > 101-O
+
+echo "<>" > 102-counting_sort.c
+
+echo "<>" > 102-O
+
+echo "<>" > 103-merge_sort.c
+
+echo "<>" > 103-O
+
+echo "<>" > 104-heap_sort.c
+
+echo "<>" > 104-O
+
+echo "<>" > 105-radix_sort.c
+
+echo "<>" > 106-bitonic_sort.c
+
+echo "<>" > 106-O
+
+echo "<>" > 107-quick_sort_hoare.c
+
+echo "<>" > 107-O
+
+echo "<>" > 1000-sort_deck.c
+
+echo "<>" > deck.h
+
+
+
+git add .
+
+git commit -m "initial commit"
+
+git push
+
+
